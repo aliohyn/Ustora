56597e1b8a0496e0428e173a6ea264c42400e8dc
Алехин Сергей <aliohyn@mail.ru>
2019-01-09 14:53:59 +0300

    Добавьте файлы проекта.

---
+++ Ustora.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ustora.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
